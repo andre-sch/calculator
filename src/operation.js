36196854f59fadcebc4e08204f98e5f8c3d4d54b
Andre Sch <andresch.dev@gmail.com>
2022-04-28 12:06:03 -0300

    Fix operation matchContentAfterSign regex

--- src/operation.js
+++ src/operation.js
@@ -2,7 +2,7 @@ const operationContainer = document.querySelector('.operation')
 
 const operation = {
   last: '',
-  matchContentAfterSign: /(?<=\s[-+×÷]).+/,
+  matchContentAfterSign: /(?<=\s[-+×÷])(?=\s).+/,
   basic: {
     addition: {
       sign: '+',
